9239b03ecde9e343849f4bda7f2e1015d474b367
Andrew Dinh <andrewdinh74@yahoo.com>
2023-10-09 15:38:49 -0700

    Turn on or off search feature based on a feature flag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
+import flagsmith from "@/utils/flagsmith";
 
-export default function Home() {
+export default async function Home() {
+  const flags = await flagsmith.getEnvironmentFlags();
   return (
     <main className="main">
       <div>
         <h2>Hello World</h2>
       </div>
       <Link href="/about">About</Link>
-      <input placeholder="Search" />
+      <br />
+      {flags.isFeatureEnabled('search') &&
+        <input placeholder="Search" />
+      }
     </main>
   );
 }
