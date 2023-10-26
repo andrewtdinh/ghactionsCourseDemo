c74e4dd52868b5728f441d2019690d75148065ae
Andrew Dinh <andrewdinh74@yahoo.com>
2023-10-25 17:03:34 -0700

    Format fix

--- src/utils/flagsmith.ts
+++ src/utils/flagsmith.ts
@@ -2,7 +2,7 @@ import Flagsmith from "flagsmith-nodejs";
 
 const flagsmith = new Flagsmith({
   environmentKey: process.env.FLAGSMITH_KEY as string,
-  requestTimeoutSeconds: 60
+  requestTimeoutSeconds: 60,
 });
 
 export default flagsmith;
